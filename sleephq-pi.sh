810b5ca8a8c63630d0e8c540446767fa3e3801f2
Erik Reynolds <erik@reynolds.io>
2023-05-09 13:34:03 -0400

    Added status statements and check for the backup directory.

--- sleephq-pi.sh
+++ sleephq-pi.sh
@@ -13,9 +13,17 @@ CARD_MOUNT_POINT="/media/erik/6263-6534"
 BACKUP_PATH="/home/erik/sleephq-backup"
 TODAY_BACKUP=$BACKUP_PATH/$(date +%Y-%m-%d)
 
-# Set the ACT LED to heartbeat
-sudo sh -c "echo heartbeat > /sys/class/leds/led0/trigger"
+echo "Checking if the backup directory exists..."
+# Make the backup directory if it doesn't exist
+if [ ! -d $BACKUP_PATH ]; then
+    echo "Backup directory doesn't exist.  Making it now..."
+    mkdir $BACKUP_PATH
+fi
 
+# Set the ACT LED to heartbeat
+sudo sh -c "echo heartbeat > /sys/class/leds/ACT/trigger"
+
+echo "Checking if the card reader is plugged in..."
 # Wait for a card reader or a camera
 CARD_READER=$(ls /dev/* | grep $CARD_DEV | cut -d"/" -f3)
 until [ ! -z $CARD_READER ]
@@ -24,19 +32,22 @@ until [ ! -z $CARD_READER ]
   CARD_READER=$(ls /dev/sd* | grep $CARD_DEV | cut -d"/" -f3)
 done
 
+echo "Checking for the SD Card mount point."
 if [ -d $CARD_MOUNT_POINT ]; then
     # Set the ACT LED to blink at 500ms to indicate that the storage device has been mounted
-    sudo sh -c "echo timer > /sys/class/leds/led0/trigger"
-    sudo sh -c "echo 500 > /sys/class/leds/led0/delay_on"
+    sudo sh -c "echo timer > /sys/class/leds/ACT/trigger"
+    sudo sh -c "echo 500 > /sys/class/leds/ACT/delay_on"
 
+    echo "Copying the SD Card contents to $BACKUP_PATH"
     # Perform back of $CARD_MOUNT_POINT to $BACKUP_PATH/current date using rsync
     rsync -avh $CARD_MOUNT_POINT/ $TODAY_BACKUP
 
+    echo "Uploading the backup to Dropbox..."
     # Upload today's backup to Dropbox
     ./dropbox_uploader.sh upload $TODAY_BACKUP
 
     # Turn off the ACT LED to indicate that the backup is completed
-    sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+    sudo sh -c "echo 0 > /sys/class/leds/ACT/brightness"
 
     # Call the Python Selenium script to upload the data to SleepHQ
 
