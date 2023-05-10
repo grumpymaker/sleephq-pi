eeacca9a8f4a570b264f19131734dc6fc89a5612
Erik Reynolds <erik@reynolds.io>
2023-05-10 10:22:16 -0400

    Working on sign in.

--- uploaddata.py
+++ uploaddata.py
@@ -7,8 +7,27 @@
 
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# My SleepHQ Credentials
+sleepUsername = ""
+sleepPassword = ""
 
 driver = webdriver.Chrome()
-driver.get('https://sleephq.com/account/teams/eglElJ')
-time.sleep(15) # Let the user actually see something!
+driver.get('https://sleephq.com/users/sign_in')
+time.sleep(5) # Let the user actually see something!
+
+username_input = driver.find_element_by_id('user_email')
+password_input = driver.find_element_by_id('user_password')
+submit_button = driver.find_element(By.TAG_NAME, 'button')
+
+username_input.send_keys(sleepUsername)
+time.sleep(2)
+password_input.send_keys(sleepPassword)
+time.sleep(2)
+submit_button.click()
+
+# automatically close the driver after 30 seconds
+time.sleep(30)
 driver.quit()
